61a2aa9a12062b6b3b4e3e1ec1eeb61e183a9f58
Kevin Moore <kevmoo@google.com>
2016-05-12 10:29:46 -0700

    Annotate tests to ensure correct browser-only behavior

--- test/oauth2_flows/implicit/utils.dart
+++ test/oauth2_flows/implicit/utils.dart
@@ -4,19 +4,5 @@
 
 import 'dart:html';
 
-// NOTE: This relies on the packages bots to checkout the package to
-// third_party/pkg/googleapis_auth.
-var DART_TESTING_URL_PREFIX =
-    '/root_dart/third_party/pkg/googleapis_auth/test/oauth2_flows/implicit';
-
-// NOTE for local testing:
-//
-// In order to run these tests without the browser controller, you can make a
-// simple *html file with a <script> tag for the test and modify the following
-// function to:
-//    String resource(String name) => name;
-String resource(String name) {
-  return Uri.parse(document.baseUri)
-      .resolve('$DART_TESTING_URL_PREFIX/$name')
-      .toString();
-}
+String resource(String name) =>
+    Uri.parse(document.baseUri).resolve(name).toString();
